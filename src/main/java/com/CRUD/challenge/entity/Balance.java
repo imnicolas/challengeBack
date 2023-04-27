abb242613897ec6d61c18c4e8f1a6ac1deb06d8c
imnicolas <nicolas.esteban.suarez@hotmail.es>
2023-04-27 18:41:40 -0300

    change atribute on table Balance

--- src/main/java/com/CRUD/challenge/entity/Balance.java
+++ src/main/java/com/CRUD/challenge/entity/Balance.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.Date;
 
 @Entity
 public class Balance {
@@ -13,12 +12,12 @@ public class Balance {
     private int id;
 
     private double valor;
-    private Date fecha;
+    private String fecha;
 
     public Balance() {
     }
 
-    public Balance(int id, double valor, Date fecha) {
+    public Balance(int id, double valor, String fecha) {
         this.id = id;
         this.valor = valor;
         this.fecha = fecha;
@@ -40,11 +39,11 @@ public class Balance {
         this.valor = valor;
     }
 
-    public Date getFecha() {
+    public String getFecha() {
         return fecha;
     }
 
-    public void setFecha(Date fecha) {
+    public void setFecha(String fecha) {
         this.fecha = fecha;
     }
 }
